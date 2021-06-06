7b089da86b81855c85ec5f6bf45adec9f415addf
Developer Java <minhxuyen2705@gmail.com>
2021-06-06 22:27:22 +0700

    config accesstoken

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,11 +1,9 @@
 import Vue from "vue"
 import axios from "axios"
 import VueAxios from "vue-axios";
-// import {ACCESS_TOKEN_ADMIN} from "../constants";
+import {ACCESS_TOKEN_ADMIN} from "../constants";
 
 Vue.use(VueAxios, axios)
-
-// const token = localStorage.getItem(ACCESS_TOKEN_ADMIN)
 
 const axiosInstance = axios.create({
     baseURL: `${process.env.VUE_APP_BASE_URL}`,
@@ -14,4 +12,10 @@ const axiosInstance = axios.create({
         // 'Authorization':'Bearer ' + token
     }
 })
+axiosInstance.interceptors.request.use(function (config) {
+    // eslint-disable-next-line no-undef
+    const token = localStorage.getItem(ACCESS_TOKEN_ADMIN);
+    config.headers.Authorization = token ? `Bearer ${token}` : '';
+    return config;
+});
 export default axiosInstance
