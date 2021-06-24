4fa28052313e5113d94aab0f37d6578662b9cbd0
duantp <duantp@vimo.vn>
2021-06-24 11:59:30 +0700

    24.06 add notiy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    port: process.env.VUE_PORT,
+    proxy: `http://localhost:${process.env.VUE_PORT}`,
+}
 }
